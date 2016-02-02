eebd653498211b584672f7532dc6bb1a27985d93
theveninanthony <ttony.97440@hotmail.fr>
2016-02-02 20:22:31 +0100

    Update MiscData.cpp
    classes coord, vertex, dir

--- WatcHom/src/Data/MiscData.cpp
+++ WatcHom/src/Data/MiscData.cpp
@@ -17,8 +17,7 @@ Axe int2Axe(int i) {
 	}
 }
 
-//surcharge d'opérateurs pour Vertex
-float& obj::Vertex::operator[](unsigned int i)
+float & obj::Vertex::at(unsigned int i)
 {
 	switch (i)
 	{
@@ -32,9 +31,16 @@ float& obj::Vertex::operator[](unsigned int i)
 		return this->z;
 		break;
 	default:
-		throw DataError("DataError: the specified component" + std::to_string(i)+ "doesn't exist");
+		throw DataError("DataError: the specified component" + std::to_string(i) + "doesn't exist");
 	}
 }
+
+//surcharge d'opérateurs pour Vertex
+float& obj::Vertex::operator[](unsigned int i)
+{
+	return this->at(i);
+}
+
 bool obj::Vertex::operator==(obj::Vertex const& b)
 {
 	//Teste si a.m_heure == b.m_heure etc.  
@@ -58,6 +64,13 @@ obj::Vertex obj::Vertex::operator+(const obj::Vertex &b)
 	result.y = this->y + b.y;
 	result.z = this->z + b.z;
 	return result;
+}
+
+obj::Vertex obj::Vertex::translation(dir xyz, float dist)
+{
+	Axe axe = xyz.first;
+	bool direct = xyz.second;
+	this->at(axe) += (direct) ? dist : -dist;
 }
 
 //surcharge d'opérateurs pour coord
@@ -92,13 +105,5 @@ obj::coord obj::coord::operator*(const int &b)
 	result.x = this->x * b;
 	result.y = this->y * b;
 	result.z = this->z * b;
-	return result;
-}
-obj::coord obj::coord::operator+(const obj::coord &b)
-{
-	obj::coord result;
-	result.x = this->x + b.x;
-	result.y = this->y + b.y;
-	result.z = this->z + b.z;
 	return result;
 }
